64f48edcada51e4c9dc4006f4de72f6b928b39d6
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-03 10:22:01 +0900

    init: Spring Boot 프로젝트 생성
    backend

---
+++ backend/trip-or-travel/src/test/java/com/pjt/triptravel/TripOrTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.triptravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripOrTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
