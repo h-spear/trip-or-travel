f3d7d595bb9f98ac6cd64a6be6be9a5d405f6fb7
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-04 15:25:16 +0900

    feat: Controller에 @Login Annotation 적용(#16)

--- backend/src/main/java/com/pjt/triptravel/board/controller/CommentController.java
+++ backend/src/main/java/com/pjt/triptravel/board/controller/CommentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import com.pjt.triptravel.board.dto.comment.CommentCreateParam;
 import com.pjt.triptravel.board.dto.comment.CommentUpdateParam;
 import com.pjt.triptravel.board.service.CommentService;
+import com.pjt.triptravel.common.configuration.annotation.Login;
 import com.pjt.triptravel.common.response.ApiResponse;
 import com.pjt.triptravel.common.session.SessionConst;
 import com.pjt.triptravel.member.entity.Member;
@@ -28,27 +29,27 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping("{postId}/comment")
-	public ApiResponse<?> write(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> write(@Login Long memberId,
 								@PathVariable Long postId,
 								@RequestBody CommentCreateParam param) {
-		Long commentId = commentService.write(member.getId(), postId, param);
+		Long commentId = commentService.write(memberId, postId, param);
 		return ApiResponse.ofSuccess(commentId);
 	}
 
 	@DeleteMapping("{postId}/comment/{commentId}")
-	public ApiResponse<?> delete(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> delete(@Login Long memberId,
 								 @PathVariable Long postId,
 								 @PathVariable Long commentId) {
-		commentService.delete(member.getId(), commentId);
+		commentService.delete(memberId, commentId);
 		return ApiResponse.ofSuccess();
 	}
 
 	@PutMapping("{postId}/comment/{commentId}")
-	public ApiResponse<?> update(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> update(@Login Long memberId,
 								 @PathVariable Long postId,
 								 @PathVariable Long commentId,
 								 @RequestBody CommentUpdateParam param) {
-		commentService.update(member.getId(), commentId, param);
+		commentService.update(memberId, commentId, param);
 		return ApiResponse.ofSuccess();
 	}
 }
--- backend/src/main/java/com/pjt/triptravel/board/controller/PostController.java
+++ backend/src/main/java/com/pjt/triptravel/board/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.pjt.triptravel.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.data.domain.Page;
@@ -24,6 +25,7 @@ import com.pjt.triptravel.board.dto.post.PostSearchCondition;
 import com.pjt.triptravel.board.dto.post.PostSearchResult;
 import com.pjt.triptravel.board.dto.post.PostUpdateParam;
 import com.pjt.triptravel.board.service.PostService;
+import com.pjt.triptravel.common.configuration.annotation.Login;
 import com.pjt.triptravel.common.response.ApiResponse;
 import com.pjt.triptravel.common.session.SessionConst;
 import com.pjt.triptravel.member.entity.Member;
@@ -50,36 +52,27 @@ public class PostController {
 	}
 
 	@PostMapping
-	public ApiResponse<?> posting(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> posting(@Login Long memberId,
 								  @RequestBody PostCreateParam param) {
-		if (member == null) {
-			return ApiResponse.ofError("로그인이 필요합니다.");
-		}
-		log.info("게시글 생성, writerId={}", member.getId());
-		Long postId = postService.posting(member.getId(), param);
+		log.info("게시글 생성, writerId={}", memberId);
+		Long postId = postService.posting(memberId, param);
 		return ApiResponse.ofSuccess(postId);
 	}
 
 	@PutMapping("/{postId}")
-	public ApiResponse<?> update(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> update(@Login Long memberId,
 								 @PathVariable Long postId,
 								 @RequestBody PostUpdateParam param) {
-		if (member == null) {
-			return ApiResponse.ofError("로그인이 필요합니다.");
-		}
-		log.info("게시글 수정, postId={}, writerId={}", postId, member.getId());
-		postService.update(postId, member.getId(), param);
+		log.info("게시글 수정, postId={}, writerId={}", postId, memberId);
+		postService.update(postId, memberId, param);
 		return ApiResponse.ofSuccess();
 	}
 
 	@DeleteMapping("/{postId}")
-	public ApiResponse<?> delete(@SessionAttribute(SessionConst.LOGIN_MEMBER) Member member,
+	public ApiResponse<?> delete(@Login Long memberId,
 								 @PathVariable Long postId) {
-		if (member == null) {
-			return ApiResponse.ofError("로그인이 필요합니다.");
-		}
-		log.info("게시글 삭제, postId={}, writerId={}", postId, member.getId());
-		postService.delete(postId, member.getId());
+		log.info("게시글 삭제, postId={}, writerId={}", postId, memberId);
+		postService.delete(postId, memberId);
 		return ApiResponse.ofSuccess();
 	}
 
