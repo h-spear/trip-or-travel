ecc6671c335ee83e450dc7282f2d71309add285f
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-04 16:52:52 +0900

    feat: 회원 관련 Controller Swagger 명세 작성(#17)

--- backend/src/main/java/com/pjt/triptravel/member/controller/AuthController.java
+++ backend/src/main/java/com/pjt/triptravel/member/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.pjt.triptravel.common.session.SessionConst;
 import com.pjt.triptravel.member.dto.LoginParam;
 import com.pjt.triptravel.member.entity.Member;
 import com.pjt.triptravel.member.service.MemberService;
+
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +21,12 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Api(tags = "Login/Logout API")
 public class AuthController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "로그인", description = "로그인을 수행합니다.")
     @PostMapping("/login")
     public ApiResponse<?> login(@RequestBody LoginParam param,
                                 HttpSession session) {
@@ -30,6 +35,7 @@ public class AuthController {
         return ApiResponse.ofSuccess();
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃을 수행합니다.")
     @PostMapping("/logout")
     public ApiResponse<?> logout(HttpSession session) {
         session.removeAttribute(SessionConst.LOGIN_MEMBER);
--- backend/src/main/java/com/pjt/triptravel/member/controller/MemberController.java
+++ backend/src/main/java/com/pjt/triptravel/member/controller/MemberController.java
@@ -1,22 +1,32 @@
 package com.pjt.triptravel.member.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.pjt.triptravel.common.response.ApiResponse;
 import com.pjt.triptravel.member.dto.MemberRegisterParam;
 import com.pjt.triptravel.member.service.MemberService;
+
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@Api(tags = "Member API")
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입", description = "사용자 계정을 생성합니다.")
     @PostMapping
     public ApiResponse<?> join(@RequestBody MemberRegisterParam param) {
         Long savedId = memberService.join(param);
         return ApiResponse.ofSuccess(savedId);
     }
 
+    // 회원정보 수정, 삭제, 마이페이지...
 }
