fea161b900599193c8ace6431da36ffead4a6169
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-13 17:41:05 +0900

    fix: Swagger 무한 로그 오류 해결(#37)

--- backend/src/main/java/com/pjt/triptravel/common/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/pjt/triptravel/common/configuration/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
+			.apis(RequestHandlerSelectors.basePackage("com.pjt.triptravel"))
+			.paths(PathSelectors.ant("/**"))
 			.build()
 			.apiInfo(apiInfo());
 	}
