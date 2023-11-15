33e9d48fcd18cee96410f175d68b7979eb825046
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-15 15:41:22 +0900

    feat: 관광지, 지역 조회 API Swagger 문서 작성(#56)

--- backend/src/main/java/com/pjt/triptravel/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/pjt/triptravel/attraction/controller/AttractionController.java
@@ -4,6 +4,8 @@ import com.pjt.triptravel.attraction.dto.attraction.AttractionAroundCondition;
 import com.pjt.triptravel.attraction.dto.attraction.AttractionSearchCondition;
 import com.pjt.triptravel.attraction.service.AttractionService;
 import com.pjt.triptravel.common.response.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,20 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/attraction")
 @RequiredArgsConstructor
+@Api(tags = "Attraction API")
 public class AttractionController {
 
     private final AttractionService attractionService;
 
+    @Operation(summary = "관광지 목록 조회", description = "관광지 목록을 조회합니다.")
     @GetMapping
     public ApiResponse<?> search(AttractionSearchCondition condition, Pageable pageable) {
         return ApiResponse.ofSuccess(attractionService.search(condition, pageable));
     }
 
+    @Operation(summary = "주변 관광지 조회", description = "현재 위도, 경도를 기준으로 특정 km 내의 관광지를 조회합니다.")
     @GetMapping("/around")
     public ApiResponse<?> around(AttractionAroundCondition condition) {
+        // 특정 거리(km)이상 조회 못하게 제약 걸어야 함
         return ApiResponse.ofSuccess(attractionService.findWithinRoundRange(condition));
     }
 
+    @Operation(summary = "관광지 상세 조회", description = "관광지 상세 정보를 조회합니다.")
     @GetMapping("/{contentId}")
     public ApiResponse<?> findOne(@PathVariable Long contentId) {
         return ApiResponse.ofSuccess(attractionService.findOne(contentId));
