eed3f4051df17b1dd85c5cb2231b70436ea65564
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-16 17:54:03 +0900

    feat: 관광지 조회 목록 API 생성(#67)

---
+++ backend/src/main/java/com/pjt/triptravel/attraction/repository/ContentTypeRepository.java
@@ -0,0 +1,7 @@
+package com.pjt.triptravel.attraction.repository;
+
+import com.pjt.triptravel.attraction.entity.ContentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentTypeRepository extends JpaRepository<ContentType, Long> {
+}
