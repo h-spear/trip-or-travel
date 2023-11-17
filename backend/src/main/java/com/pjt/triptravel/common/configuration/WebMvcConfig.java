2c58f24f35c873af3ce22e8f0abc758a0c2b148e
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-17 09:42:03 +0900

    fix: CORS 설정 필터 방식으로 변경

--- backend/src/main/java/com/pjt/triptravel/common/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/pjt/triptravel/common/configuration/WebMvcConfig.java
@@ -2,26 +2,26 @@ package com.pjt.triptravel.common.configuration;
 
 import java.util.List;
 
+import com.pjt.triptravel.common.configuration.filter.CorsFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.pjt.triptravel.common.configuration.argumentResolver.LoginMemberArgumentResolver;
 
+import javax.servlet.Filter;
+
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(new LoginMemberArgumentResolver());
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:9000")
-				.allowCredentials(true)
-				.maxAge(3600);
 	}
 }
