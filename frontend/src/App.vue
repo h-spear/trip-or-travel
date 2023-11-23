edaf652b43504dc0a56e93061cfbbde62a07b79b
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-23 17:04:14 +0900

    feat: 챗봇 라이브러리 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,46 @@ import TheHeadingNavbar from '@/components/layout/TheHeadingNavbar.vue';
 import { AddressStore } from '@/stores/AddressStore.js';
 const addressStore = AddressStore();
 const { sidos, funcGetItems } = addressStore;
+
+(function () {
+  var w = window;
+  if (w.ChannelIO) {
+    return w.console.error('ChannelIO script included twice.');
+  }
+  var ch = function () {
+    ch.c(arguments);
+  };
+  ch.q = [];
+  ch.c = function (args) {
+    ch.q.push(args);
+  };
+  w.ChannelIO = ch;
+  function l() {
+    if (w.ChannelIOInitialized) {
+      return;
+    }
+    w.ChannelIOInitialized = true;
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+    var x = document.getElementsByTagName('script')[0];
+    if (x.parentNode) {
+      x.parentNode.insertBefore(s, x);
+    }
+  }
+  if (document.readyState === 'complete') {
+    l();
+  } else {
+    w.addEventListener('DOMContentLoaded', l);
+    w.addEventListener('load', l);
+  }
+})();
+
+ChannelIO('boot', {
+  pluginKey: 'b1c0db0c-a76b-4219-a7ba-8059360f9001' // fill your plugin key
+});
+
 onMounted(() => {
   if (!sidos.value) {
     funcGetItems();
@@ -11,7 +51,8 @@ onMounted(() => {
 });
 </script>
 
-<template>z``
+<template>
+  z``
   <div style="display: flex; justify-content: center; align-items: center">
     <TheHeadingNavbar></TheHeadingNavbar>
     <router-view></router-view>
