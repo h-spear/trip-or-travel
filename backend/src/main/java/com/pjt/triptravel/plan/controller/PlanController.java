14a4fbee54f1d487651374831023d3f8c1826aff
h-spear <80024307+h-spear@users.noreply.github.com>
2023-11-24 10:45:37 +0900

    feat: PlanControlle Swagger 문서 작성

--- backend/src/main/java/com/pjt/triptravel/plan/controller/PlanController.java
+++ backend/src/main/java/com/pjt/triptravel/plan/controller/PlanController.java
@@ -6,6 +6,7 @@ import com.pjt.triptravel.plan.dto.PlanCreateParam;
 import com.pjt.triptravel.plan.dto.PlanUpdateParam;
 import com.pjt.triptravel.plan.service.PlanService;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,8 @@ import org.springframework.web.bind.annotation.*;
 public class PlanController {
     
     private final PlanService planService;
-    
+
+    @Operation(summary = "여행 계획 생성", description = "여행 계획을 생성합니다.")
     @PostMapping
     public ApiResponse<?> create(@Login Long memberId,
                                  @RequestBody PlanCreateParam param) {
@@ -29,12 +31,14 @@ public class PlanController {
         return ApiResponse.ofSuccess(planId);
     }
 
+    @Operation(summary = "여행 계획 조회", description = "자신의 여행 계획 목록을 조회합니다.")
     @GetMapping
     public ApiResponse<?> list(@Login Long memberId) {
         log.info("여행 계획 조회, memberId={}", memberId);
         return ApiResponse.ofSuccess(planService.getPlans(memberId));
     }
 
+    @Operation(summary = "여행 계획 상세 조회", description = "planId에 해당하는 여행 계획을 상세 조회합니다.")
     @GetMapping("/{planId}")
     public ApiResponse<?> detail(@Login Long memberId,
                                  @PathVariable Long planId) {
@@ -42,6 +46,7 @@ public class PlanController {
         return ApiResponse.ofSuccess(planService.getDetail(memberId, planId));
     }
 
+    @Operation(summary = "여행 계획 수정", description = "planId에 해당하는 여행 계획을 수정합니다.")
     @PutMapping("/{planId}")
     public ApiResponse<?> modify(@Login Long memberId,
                                  @PathVariable Long planId,
@@ -51,6 +56,7 @@ public class PlanController {
         return ApiResponse.ofSuccess();
     }
 
+    @Operation(summary = "여행 계획 삭제", description = "planId에 해당하는 여행 계획을 삭제합니다.")
     @DeleteMapping("/{planId}")
     public ApiResponse<?> delete(@Login Long memberId,
                                  @PathVariable Long planId) {
